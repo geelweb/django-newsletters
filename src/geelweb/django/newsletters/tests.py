2e899b03a86ffedfabdcb5332169f030ada9af01
Guillaume Luchet <guillaume@geelweb.org>
2015-04-21 13:17:47 +0200

    Add basic unit tests

--- src/geelweb/django/newsletters/tests.py
+++ src/geelweb/django/newsletters/tests.py
@@ -1,3 +1,19 @@
+from django.db import IntegrityError
 from django.test import TestCase
 
-# Create your tests here.
+from geelweb.django.newsletters.models import Newsletter
+
+class NewsletterTest(TestCase):
+    def test_uniqueness(self):
+        item = Newsletter.objects.create(email="john@example.com")
+        with self.assertRaises(IntegrityError):
+            item = Newsletter.objects.create(email="john@example.com")
+
+    def test_defaults(self):
+        item = Newsletter.objects.create(email="john@example.com")
+        self.assertTrue(item.optin)
+
+    def test_unicode(self):
+        item = Newsletter.objects.create(email="john@example.com")
+        self.assertEqual(str(item), "john@example.com")
+
