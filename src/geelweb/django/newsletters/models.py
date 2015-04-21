5000bbd48a725d4bc68caeb2ed496c6848ecc11e
Guillaume Luchet <guillaume@geelweb.org>
2015-04-21 13:16:46 +0200

    replace __unicode__ method by __str__ #python3 #compatibility

--- src/geelweb/django/newsletters/models.py
+++ src/geelweb/django/newsletters/models.py
@@ -1,6 +1,8 @@
 from django.db import models
+from django.utils.encoding import python_2_unicode_compatible
 from django.utils.translation import ugettext as _
 
+@python_2_unicode_compatible
 class Newsletter(models.Model):
     email = models.EmailField(unique=True, error_messages={'unique': _('This email is already registered')})
     date_created = models.DateTimeField(auto_now=False, auto_now_add=True)
@@ -10,5 +12,5 @@ class Newsletter(models.Model):
         verbose_name = _('email')
         verbose_name_plural = _('emails')
 
-    def __unicode__(self):
+    def __str__(self):
         return self.email
